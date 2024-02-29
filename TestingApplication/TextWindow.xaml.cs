014d06685556927b832091827838a7b58190a253
Anjali <anjalisheel@microsoft.com>
2024-02-29 18:06:30 +0530

    Modified tests

--- TestingApplication/TextWindow.xaml.cs
+++ TestingApplication/TextWindow.xaml.cs
@@ -1,22 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using static System.Net.Mime.MediaTypeNames;
-using TestingApplication.Models;
-using System.Reflection;
-using Application = System.Windows.Application;
-using System.Drawing;
-using System.Xml;
 using TestingApplication.Models;
 
 namespace TestingApplication
---
+++ Win11ThemeTest/TextTest.cs
@@ -0,0 +1,49 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Input;
+using FlaUI.UIA3;
+using TestingApplication;
+using TestingApplication.Models;
+
+namespace Win11ThemeTest
+{
+    [TestFixture]
+    public class TextTest
+    {
+        private Application app;
+        private Window mainWindow;
+        private Window textWindow;
+        TextBox textBox;
+        TextBox disabledTextBox;
+        Button txtButton;
+
+        public TextTest()
+        {
+            app = Application.Launch(@"..\\..\\..\\..\\TestingApplication\\bin\\Debug\\net9.0-windows\\win-x64\\TestingApplication.exe");
+
+            using (var automation = new UIA3Automation())
+            {
+                mainWindow = app.GetMainWindow(automation);
+                txtButton = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("txtBoxButton")).AsButton();
+                Mouse.Click(txtButton.GetClickablePoint());
+                Wait.UntilInputIsProcessed(TimeSpan.FromMilliseconds(500));
+                textWindow = mainWindow.FindFirstDescendant(cf => cf.ByName("TextWindow")).AsWindow();
+                textBox = textWindow.FindFirstDescendant(cf => cf.ByAutomationId("tbTxt")).AsTextBox();
+                var textBox1 = textWindow.FindFirstDescendant(cf => cf.ByAutomationId("tbTxt")).AsTextBox();
+                disabledTextBox = textWindow.FindFirstDescendant(cf => cf.ByAutomationId("tbTxt_disabled")).AsTextBox();
+            }
+        }
+
+        [Test]
+        public void TextSelection()
+        {
+            UIProperties properties = new UIProperties();
+            var borderThickness = properties.BorderThickness;
+
+            Console.WriteLine();
+            textBox.Enter("Hello World!");
+            Wait.UntilInputIsProcessed(TimeSpan.FromMilliseconds(500));
+            textWindow = mainWindow.FindFirstDescendant(cf => cf.ByName("TextWindow")).AsWindow();
+        }
+    }
+}
